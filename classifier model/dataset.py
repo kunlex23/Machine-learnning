5216d1cdc23f8e33c4e52df3743a73953581faa6
kunlex23 <kunlex23@gmail.com>
2023-11-09 17:41:53 +0100

    keras

--- classifier model/dataset.py
+++ classifier model/dataset.py
@@ -2,6 +2,35 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from scipy.spatial import distance 
+
+def euc (a,b):
+    return distance.euclidean(a,b)
+
+class testKNN():
+    def fit(self, features_train, labels_train):
+        self.features_train = features_train
+        self.labels_train = labels_train
+
+    def predict(self, features_test):
+        prediction = []
+        for item in features_test:
+            label = self.closet(item)
+            prediction.append(label)
+
+        return prediction
+
+    def closet (self, item):
+        best_dist = euc(item, self.features_train[0])
+        best_index = 0
+        for i in range(1, len(self.features_train)):
+            dist = euc(item, self.features_train[i])
+            if dist < best_dist:
+                best_dist = dist 
+                best_index = i
+
+        return self.labels_train[best_index]
+                
 
 iris = datasets.load_iris()
 
@@ -10,9 +39,10 @@ label = iris.target
 
 features_train, features_test, labels_train, labels_test = train_test_split(features, label, test_size = .5)
 
-my_classifier = KNeighborsClassifier()
+my_classifier = testKNN()
+# my_classifier = KNeighborsClassifier()
 my_classifier.fit(features_train, labels_train)
-
+ 
 prediction = my_classifier.predict(features_test)
 
 print(accuracy_score(labels_test,prediction))
---
+++ classifier model/handw-classifier.py
@@ -0,0 +1,4 @@
+import keras
+from kera.dataset import mnist
+
+data = mnist.load_data()
